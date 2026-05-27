d854c57ac8d615bca0857a754261c878b534887f
gedion <gedion.basson@gmail.com>
2026-05-27 10:08:44 +0200

    Building a new Mobile Stock Capture Application, Just created the UI.

---
+++ SectionC-MobileStockCaptureApp/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace SectionC_MobileStockCaptureApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblOutput = new Label();
+            lblCode = new Label();
+            lblMake = new Label();
+            lblQuantity = new Label();
+            txtCode = new TextBox();
+            txtMake = new TextBox();
+            txtQuantity = new TextBox();
+            btnAdd = new Button();
+            btnDelete = new Button();
+            btnFind = new Button();
+            SuspendLayout();
+            // 
+            // lblOutput
+            // 
+            lblOutput.AutoSize = true;
+            lblOutput.BorderStyle = BorderStyle.FixedSingle;
+            lblOutput.Location = new Point(545, 21);
+            lblOutput.Name = "lblOutput";
+            lblOutput.Size = new Size(2, 22);
+            lblOutput.TabIndex = 0;
+            // 
+            // lblCode
+            // 
+            lblCode.AutoSize = true;
+            lblCode.Location = new Point(385, 117);
+            lblCode.Name = "lblCode";
+            lblCode.Size = new Size(98, 20);
+            lblCode.TabIndex = 1;
+            lblCode.Text = "Mobile Code:";
+            // 
+            // lblMake
+            // 
+            lblMake.AutoSize = true;
+            lblMake.Location = new Point(385, 179);
+            lblMake.Name = "lblMake";
+            lblMake.Size = new Size(48, 20);
+            lblMake.TabIndex = 2;
+            lblMake.Text = "Make:";
+            // 
+            // lblQuantity
+            // 
+            lblQuantity.AutoSize = true;
+            lblQuantity.Location = new Point(385, 243);
+            lblQuantity.Name = "lblQuantity";
+            lblQuantity.Size = new Size(68, 20);
+            lblQuantity.TabIndex = 3;
+            lblQuantity.Text = "Quantity:";
+            // 
+            // txtCode
+            // 
+            txtCode.Location = new Point(533, 117);
+            txtCode.Name = "txtCode";
+            txtCode.Size = new Size(125, 27);
+            txtCode.TabIndex = 4;
+            // 
+            // txtMake
+            // 
+            txtMake.Location = new Point(533, 179);
+            txtMake.Name = "txtMake";
+            txtMake.Size = new Size(125, 27);
+            txtMake.TabIndex = 5;
+            // 
+            // txtQuantity
+            // 
+            txtQuantity.Location = new Point(533, 243);
+            txtQuantity.Name = "txtQuantity";
+            txtQuantity.Size = new Size(125, 27);
+            txtQuantity.TabIndex = 6;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(339, 315);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(94, 29);
+            btnAdd.TabIndex = 7;
+            btnAdd.Text = "Add";
+            btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(509, 315);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(94, 29);
+            btnDelete.TabIndex = 8;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnFind
+            // 
+            btnFind.Location = new Point(665, 315);
+            btnFind.Name = "btnFind";
+            btnFind.Size = new Size(94, 29);
+            btnFind.TabIndex = 9;
+            btnFind.Text = "Find";
+            btnFind.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1178, 488);
+            Controls.Add(btnFind);
+            Controls.Add(btnDelete);
+            Controls.Add(btnAdd);
+            Controls.Add(txtQuantity);
+            Controls.Add(txtMake);
+            Controls.Add(txtCode);
+            Controls.Add(lblQuantity);
+            Controls.Add(lblMake);
+            Controls.Add(lblCode);
+            Controls.Add(lblOutput);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblOutput;
+        private Label lblCode;
+        private Label lblMake;
+        private Label lblQuantity;
+        private TextBox txtCode;
+        private TextBox txtMake;
+        private TextBox txtQuantity;
+        private Button btnAdd;
+        private Button btnDelete;
+        private Button btnFind;
+    }
+}
